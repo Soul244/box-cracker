623dfe1ae4ee7db97ee55769d382f9ac1aca08a5
OmerCD <omercd@hotmail.com.tr>
2016-07-02 11:33:03 +0300

    Patlama Efekti
    *Bazı düzenlemeler yapıldı.

---
+++ Assets/Kodlar/TestKodu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestKodu : MonoBehaviour {
+
+    public GameObject küp;
+    public GameObject patlamaEfekti;
+    ParticleSystem patlama;
+    GameObject yokEdilecekPatlamaEfekti;
+    void Start()
+    {
+        Instantiate(küp,küp.transform.position,Quaternion.identity);
+    }
+    void OnMouseUpAsButton()
+    {
+        Instantiate(patlamaEfekti, küp.transform.position, Quaternion.identity);
+    }
+    void Update()
+    {
+    }
+}
