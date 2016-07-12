054f40fb310f8bb7636d8faef174219cca862d6e
Soul244 <hasancangedik@hotmail.com>
2016-07-13 00:11:04 +0300

    Arkaplan Değişiklikliği
    * Oyun arkaplanı değiştirdim. Hoş ne yaptım ben de tam anlamadım.

---
+++ Assets/Kodlar/ArkaplanKaydır2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ArkaplanKaydır2 : MonoBehaviour {
+    public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+
+    }
+	// Update is called once per frame
+	void Update () {
+        Vector2 offset = new Vector2(0, Time.time *speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
