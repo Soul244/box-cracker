70bcc22d3cd80ffe54f9cd4574fbc550296dcb8d
Soul244 <hasancangedik@hotmail.com>
2016-07-14 13:57:45 +0200

    * Reklam Eklendi
    - Ama çalışmıyor :dancer:

---
+++ Assets/Kodlar/Reklam.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds.Api;
+
+public class Reklam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BannerView reklamObjesi = new BannerView(
+            "ca-app-pub-2207285899275971/8901820447", AdSize.SmartBanner, AdPosition.Bottom);
+        AdRequest reklamiAl = new AdRequest.Builder().Build();
+        reklamObjesi.LoadAd(reklamiAl);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
