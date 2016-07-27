0137eba35cf266459a537b2035f8bf3ac94ab69a
Soul244 <hasancangedik@hotmail.com>
2016-07-28 00:42:03 +0300

    Başarım Sistemi ve LeaderBoards Eklendi
    Leaderboards ekranı açılmadı niyeyse. Sonra bakarım.

--- Assets/Kodlar/Menü/BaşarımSayfasıAç.cs
+++ Assets/Kodlar/Menü/BaşarımSayfasıAç.cs
@@ -1,9 +1,26 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
-
+using GooglePlayGames;
 public class BaşarımSayfasıAç : MonoBehaviour {
-public void SayfaAç()
+    int tkutu;
+    public string Başarım1;
+    void Awake()
     {
+        PlayGamesPlatform.Activate();
+        PlayGamesPlatform.DebugLogEnabled = true;
+        tkutu = PlayerPrefs.GetInt("ToplamPatlatılanKutu");
+    }
+    void Start()
+    {
+        Social.localUser.Authenticate(
+            ( bool success) => {
+            });
+        if (tkutu>1000)
+        {
+            Social.ReportProgress(Başarım1, 100.0f, (bool success) => { });
+        }
+    }
+    public void SayfaAç()
+    {
+            Social.ShowAchievementsUI();
     }
 }
---
+++ Assets/Kodlar/Menü/GoogleSkor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+public class GoogleSkor : MonoBehaviour {
+
+    // Use this for initialization
+    void Awake()
+    {
+        PlayGamesPlatform.Activate();
+        PlayGamesPlatform.DebugLogEnabled = true;
+    }
+    void Start () {
+        Social.localUser.Authenticate(
+        (bool success) => {
+        });
+        Social.ReportScore(PlayerPrefs.GetInt("High Score"), "CgkI1p743fUHEAIQAw", (bool success) => {
+            // handle success or failure
+        });
+    }
+
+    public void skorGöster()
+    {
+        Social.ShowAchievementsUI();
+    }
+}
