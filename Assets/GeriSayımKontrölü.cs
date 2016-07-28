a7361e8f56680ab788ca2710dc5951e32c4b5584
OmerCD <omercd@hotmail.com.tr>
2016-07-28 18:29:34 +0300

    Geri Sayım
    * Geri Sayım stili değiştirildi

--- Assets/GeriSayımKontrölü.cs
+++ Assets/GeriSayımKontrölü.cs
@@ -6,12 +6,15 @@ public class GeriSayımKontrölü : MonoBehaviour {
     Text yazıAlanı;
     float zaman = 3f;
     Canvas araMenü;
+    Image arkaplanResmi;
+    float hız;
 	// Use this for initialization
 	void Start () {
+        arkaplanResmi = transform.GetComponentInParent<Image>();
         yazıAlanı = GetComponent<Text>();
-        
+        AraMenüFonksiyonları.oyunDurdu = true;
         araMenü = GameObject.Find("Ara Menü").GetComponent<Canvas>();
-
+        hız = 0.025f;
     }
 	
 	// Update is called once per frame
@@ -19,13 +22,22 @@ public class GeriSayımKontrölü : MonoBehaviour {
         if (!araMenü.enabled)
         {
             zaman -= Time.deltaTime;
-            AraMenüFonksiyonları.oyunDurdu = true;
+            arkaplanResmi.fillAmount = zaman / 3;
+
             if (zaman <= 0)
             {
                 AraMenüFonksiyonları.oyunDurdu = false;
-                Destroy(this.gameObject);
+                Destroy(transform.parent.parent.gameObject);
             }
-            yazıAlanı.text = zaman.ToString("0.0");
+            else if (zaman <= 1.5f)
+            {
+                arkaplanResmi.color = Color.Lerp(arkaplanResmi.color, Color.red, hız);
+            }
+            else if (zaman<=2.5f)
+            {
+                arkaplanResmi.color = Color.Lerp(arkaplanResmi.color, Color.yellow, hız);
+            }
+            yazıAlanı.text = zaman.ToString("0");
         }
     }
 }
