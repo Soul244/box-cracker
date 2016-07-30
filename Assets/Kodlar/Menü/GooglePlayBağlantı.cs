d14d0adcc2a761f0674ccdb13ae0db9912effde4
OmerCD <omercd@hotmail.com.tr>
2016-07-30 14:07:35 +0300

    Hata Mesajı
    * Ana menüye bağlantı yok hatası için mesaj koyuldu.

--- Assets/Kodlar/Menü/GooglePlayBağlantı.cs
+++ Assets/Kodlar/Menü/GooglePlayBağlantı.cs
@@ -7,8 +7,14 @@ using System;
 
 public class GooglePlayBağlantı : MonoBehaviour
 {
+    GUIStyle centeredStyle;
+    bool mesajGöster = false;
+    float mesajZamanlayıcı = 6f;
+    public Texture2D mesajArkaplan;
     void Start()
     {
+        mesajGöster = false;
+
         PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
 
      .Build();
@@ -51,7 +57,23 @@ public class GooglePlayBağlantı : MonoBehaviour
 
     private void BağlantıYokMesajı()
     {
-        
+        mesajGöster = true;
+    }
+    void BağlantıYokMesajıGöster()
+    {
+        centeredStyle = GUI.skin.GetStyle("Label");
+        centeredStyle.alignment = TextAnchor.UpperCenter;
+        GUI.Label(new Rect(Screen.width / 2 - 87.5f, Screen.height - 90, 175, 50), mesajArkaplan, centeredStyle);
+        mesajZamanlayıcı -= Time.deltaTime;
+        mesajGöster = mesajZamanlayıcı > 0;
+        mesajZamanlayıcı = mesajGöster ? mesajZamanlayıcı : 6f;
+    }
+    public void OnGUI()
+    {
+        if (mesajGöster)
+        {
+            BağlantıYokMesajıGöster();
+        }
     }
 }
 
