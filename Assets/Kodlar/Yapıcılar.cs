f60362d1c8084a460243e85dae2948e457973943
OmerCD <omercd@hotmail.com.tr>
2016-08-03 19:52:27 +0300

    Ufak Düzeltmeler

---
+++ Assets/Kodlar/Yapıcılar.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Yapıcılar : MonoBehaviour {
+
+public void OnMouseUpAsButton()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
