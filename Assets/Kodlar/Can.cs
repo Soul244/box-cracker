6cc68777d4c92b8b2ee0b7ba5c84983122c87498
Soul244 <hasancangedik@hotmail.com>
2016-09-06 22:21:18 +0300

    Can Düzeltmesi
    Sorun muhtemelen düzeltildi.

--- Assets/Kodlar/Can.cs
+++ Assets/Kodlar/Can.cs
@@ -9,7 +9,7 @@ public class Can : MonoBehaviour
     public Text süreYazı;
     float zaman = 0f;
     int cansayisi = 0;
-    float süresayisi = 300f;
+    int süresayisi = 300;
     int gecici = 0;
     int dk, sn = 0;
     // Use this for initialization
@@ -18,14 +18,15 @@ public class Can : MonoBehaviour
         if (PlayerPrefs.GetInt("Can Sayısı") != 10)
         {
             süresayisi = PlayerPrefs.GetInt("Süre");
-            süreYazı.text = süresayisi.ToString();
+            Cevirici(süresayisi);
         }
         else
         {
             süreYazı.text = "Filled";
         }
-        canYazı.text = PlayerPrefs.GetInt("Can Sayısı").ToString();
         cansayisi = PlayerPrefs.GetInt("Can Sayısı");
+        canYazı.text = cansayisi.ToString();
+        InvokeRepeating("CanArttir", 1,1);
     }
     void OnApplicationPause(bool durum)
     {
@@ -34,6 +35,7 @@ public class Can : MonoBehaviour
             TimeSpan ts = DateTime.Now.Subtract(DateTime.Parse(PlayerPrefs.GetString("Kapanıs")));
             OyunDevamEtti((int)ts.TotalSeconds);
         }
+
     }
     public void OyunDevamEtti(int eklenenSüre)
     {
@@ -64,56 +66,58 @@ public class Can : MonoBehaviour
                 else
                 {
                     PlayerPrefs.SetInt("Can Sayısı", PlayerPrefs.GetInt("Can Sayısı") + can);
-                    PlayerPrefs.SetInt("Süre", öncekisüre - eksisüre);
+                    PlayerPrefs.SetInt("Süre", öncekisüre - eksisüre); 
                 }
             }
+            süresayisi = PlayerPrefs.GetInt("Süre");
         }
     }
-    // Update is called once per frame
-    void Update()
+    void CanArttir()
     {
         if (cansayisi != 10)
-        {
-            zaman += Time.deltaTime;
-            gecici = Convert.ToInt32(süresayisi - int.Parse(zaman.ToString().Split('.')[0]));
-            if (gecici > 59)
-            {
-                dk = gecici / 60;
-                sn = gecici % 60;
-            }
-            else
-            {
-                dk = 0;
-                sn = gecici;
-            }
-            if (sn == 0)
-            {
-                süreYazı.text = dk.ToString() + ":00";
-                PlayerPrefs.SetInt("Süre", gecici);
-            }
-            else if (sn < 10 && sn > 0)
-            {
-                süreYazı.text = dk.ToString() + ":0" + sn.ToString();
-                PlayerPrefs.SetInt("Süre", gecici);
-            }
-            else
-            {
-                süreYazı.text = dk.ToString() + ":" + sn.ToString();
-                PlayerPrefs.SetInt("Süre", gecici);
-            }
+        {           
+            zaman += 1;
+            gecici = Convert.ToInt32(süresayisi - zaman);
+            Cevirici(gecici);
             if (zaman >= süresayisi)
             {
                 cansayisi++;
                 canYazı.text = (cansayisi).ToString();
                 PlayerPrefs.SetInt("Can Sayısı", cansayisi);
-                süresayisi = 300f;
                 zaman = 0;
             }
+            PlayerPrefs.SetInt("Süre", gecici);
             cansayisi = PlayerPrefs.GetInt("Can Sayısı");
         }
         else
         {
+            PlayerPrefs.SetInt("Süre", süresayisi);
             süreYazı.text = "Filled";
+        }
+    }
+    void Cevirici(int gecici)
+    {
+        if (gecici > 59)
+        {
+            dk = gecici / 60;
+            sn = gecici % 60;
+        }
+        else
+        {
+            dk = 0;
+            sn = gecici;
+        }
+        if (sn == 0)
+        {
+            süreYazı.text = dk.ToString() + ":00";
+        }
+        else if (sn < 10 && sn > 0)
+        {
+            süreYazı.text = dk.ToString() + ":0" + sn.ToString();
+        }
+        else
+        {
+            süreYazı.text = dk.ToString() + ":" + sn.ToString();
         }
     }
 }
--- Assets/Kodlar/Can2.cs
+++ Assets/Kodlar/Can2.cs
@@ -6,28 +6,29 @@ using System;
 public class Can2 : MonoBehaviour {
     float zaman = 0f;
     int cansayisi = 0;
-    float süresayisi = 300f;
+    int süresayisi = 300;
     int gecici = 0;
 	// Use this for initialization
 	void Start () {
-        if (PlayerPrefs.GetInt("Süre")>0)
+        if (PlayerPrefs.GetInt("Can Sayısı") != 10)
         {
             süresayisi = PlayerPrefs.GetInt("Süre");
         }
-    }	
-	// Update is called once per frame
-	void Update () {
+        InvokeRepeating("CanArttir2", 1,1);
+    }
+    void CanArttir2()
+    {
         cansayisi = PlayerPrefs.GetInt("Can Sayısı");
         if (cansayisi != 10)
         {
-            zaman += Time.deltaTime;
-            gecici = Convert.ToInt32(süresayisi - int.Parse(zaman.ToString().Split('.')[0]));
+            zaman += 1;
+            gecici = Convert.ToInt32(süresayisi - zaman);
             PlayerPrefs.SetInt("Süre", gecici);
             if (zaman >= süresayisi)
             {
                 cansayisi++;
                 PlayerPrefs.SetInt("Can Sayısı", cansayisi);
-                süresayisi = 300f;
+                süresayisi = 300;
                 zaman = 0;
             }
         }
